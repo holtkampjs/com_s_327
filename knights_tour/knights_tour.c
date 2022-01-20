3143a80f78ded9b76368d01d58bec614994cabf0
jholt <holtkampjs@gmail.com>
2022-01-19 18:50:52 -0600

    Added a dummy statement

--- knights_tour/knights_tour.c
+++ knights_tour/knights_tour.c
@@ -70,6 +70,8 @@ int solve_knights_tour_recur(int x, int y, int moveNum, struct move knightMoves[
 
     for (i = 0; i < KNIGHT_MOVES; i++)
     {
+        if (i == 7)
+            printf("welp");
         nextX = x + knightMoves[i].x;
         nextY = y + knightMoves[i].y;
 
