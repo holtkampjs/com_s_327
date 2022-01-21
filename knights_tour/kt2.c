78d6db6470af28ac2ffc3d00ad23634f707bcf62
jholt <holtkampjs@gmail.com>
2022-01-20 23:13:53 -0600

    Made path a global pointer variable

--- knights_tour/kt2.c
+++ knights_tour/kt2.c
@@ -10,6 +10,7 @@ typedef struct _coordinate
 } vector;
 
 int board[5][5];
+int *path;
 vector moves[] = {{-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {2, 1}, {1, 2}};
 
 int is_valid_move(int x, int y)
@@ -17,14 +18,14 @@ int is_valid_move(int x, int y)
     return x >= 0 && x < 5 && y >= 0 && y < 5 && board[y][x] == 0;
 }
 
-int show_tour(int *tour, int len)
+int show_tour(int len)
 {
     int i;
     for (i = 0; i < len; i++)
         if (i == len - 1)
-            printf("%d\n", tour[i]);
+            printf("%d\n", path[i]);
         else
-            printf("%d,", tour[i]);
+            printf("%d,", path[i]);
 
     return 1;
 }
@@ -43,28 +44,28 @@ void print_board()
     printf("\n");
 }
 
-int knights_tour_util(int x, int y, int move, int *tour)
+int knights_tour_util(int x, int y, int move)
 {
     int i, xMove, yMove;
     int flag = 0;
 
     board[y][x] = move + 1;
 
-    tour[move] = 5 * y + x + 1;
+    path[move] = 5 * y + x + 1;
 
     if (move == 24)
-        return show_tour(tour, move + 1);
+        return show_tour(move + 1);
 
     for (i = 0; i < NUM_MOVES; i++)
     {
         xMove = x + moves[i].x;
         yMove = y + moves[i].y;
 
-        if (is_valid_move(xMove, yMove) && knights_tour_util(xMove, yMove, move + 1, tour))
+        if (is_valid_move(xMove, yMove) && knights_tour_util(xMove, yMove, move + 1))
             flag = 1;
     }
 
-    tour[move] = 0;
+    path[move] = 0;
     board[y][x] = 0;
 
     return flag;
@@ -82,13 +83,14 @@ void solve_knights_tour()
 {
     int i, j;
     int failure = 1;
-    int *path = (int *)malloc(25 * sizeof(int));
+
+    path = (int *)malloc(25 * sizeof(int));
 
     init_board();
 
     for (i = 0; i < 5; i++)
         for (j = 0; j < 5; j++)
-            if (knights_tour_util(j, i, 0, path))
+            if (knights_tour_util(j, i, 0))
                 failure = 0;
 
     if (failure)
