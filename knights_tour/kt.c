def620737e0839c7807a2fe3537738c4fab89815
jholt <holtkampjs@gmail.com>
2022-01-25 09:51:19 -0600

    Updated makefile

--- knights_tour/kt.c
+++ knights_tour/kt.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #define NUM_MOVES 8
+#define DIM 5
 
 typedef struct _coordinate
 {
@@ -9,7 +10,7 @@ typedef struct _coordinate
     int y;
 } vector;
 
-int board[5][5];
+int board[DIM][DIM];
 int *path;
 vector moves[] = {{-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {2, 1}, {1, 2}};
 
@@ -17,7 +18,7 @@ int count = 0;
 
 int is_valid_move(int x, int y)
 {
-    return x >= 0 && x < 5 && y >= 0 && y < 5 && board[y][x] == 0;
+    return x >= 0 && x < DIM && y >= 0 && y < DIM && board[y][x] == 0;
 }
 
 int show_tour(int len)
@@ -38,9 +39,9 @@ void print_board()
 {
     int i, j;
 
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < DIM; i++)
     {
-        for (j = 0; j < 5; j++)
+        for (j = 0; j < DIM; j++)
             printf(" %2d ", board[i][j]);
         printf("\n");
     }
@@ -55,9 +56,9 @@ int knights_tour_util(int x, int y, int move)
 
     board[y][x] = move + 1;
 
-    path[move] = 5 * y + x + 1;
+    path[move] = DIM * y + x + 1;
 
-    if (move == 24)
+    if (move == DIM * DIM - 1)
         flag = show_tour(move + 1);
     else
         for (i = 0; i < NUM_MOVES; i++)
@@ -78,8 +79,8 @@ int knights_tour_util(int x, int y, int move)
 void init_board()
 {
     int i, j;
-    for (i = 0; i < 5; i++)
-        for (j = 0; j < 5; j++)
+    for (i = 0; i < DIM; i++)
+        for (j = 0; j < DIM; j++)
             board[i][j] = 0;
 }
 
@@ -88,12 +89,12 @@ void solve_knights_tour()
     int i, j;
     int success = 0;
 
-    path = (int *)malloc(25 * sizeof(int));
+    path = (int *)malloc(DIM * DIM * sizeof(int));
 
     init_board();
 
-    for (i = 0; i < 5; i++)
-        for (j = 0; j < 5; j++)
+    for (i = 0; i < DIM; i++)
+        for (j = 0; j < DIM; j++)
             if (knights_tour_util(j, i, 0))
                 success = 1;
 
