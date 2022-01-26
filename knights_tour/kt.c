4192b67e8f3c3df1f33d77f4465c66d67dc0b685
jholt <holtkampjs@gmail.com>
2022-01-25 20:39:23 -0600

    Refactored and added functionality

--- knights_tour/kt.c
+++ knights_tour/kt.c
@@ -13,39 +13,32 @@ typedef struct _coordinate
 int board[DIM][DIM];
 int *path;
 vector moves[] = {{-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {2, 1}, {1, 2}};
-
-int count = 0;
+int visual = 0, count = 0;
 
 int is_valid_move(int x, int y)
 {
     return x >= 0 && x < DIM && y >= 0 && y < DIM && board[y][x] == 0;
 }
 
-int show_tour(int len)
+void show_tour(int len)
 {
     int i;
-
     for (i = 0; i < len; i++)
         if (i == len - 1)
             printf("%d\n", path[i]);
         else
             printf("%d,", path[i]);
-
-    count++;
-    return 1;
 }
 
 void print_board()
 {
     int i, j;
-
     for (i = 0; i < DIM; i++)
     {
         for (j = 0; j < DIM; j++)
             printf(" %2d ", board[i][j]);
         printf("\n");
     }
-
     printf("\n");
 }
 
@@ -55,17 +48,22 @@ int knights_tour_util(int x, int y, int move)
     int flag = 0;
 
     board[y][x] = move + 1;
-
     path[move] = DIM * y + x + 1;
 
     if (move == DIM * DIM - 1)
-        flag = show_tour(move + 1);
+    {
+        if (visual)
+            print_board();
+        else
+            show_tour(move + 1);
+        flag = 1;
+        count++;
+    }
     else
         for (i = 0; i < NUM_MOVES; i++)
         {
             xMove = x + moves[i].x;
             yMove = y + moves[i].y;
-
             if (is_valid_move(xMove, yMove) && knights_tour_util(xMove, yMove, move + 1))
                 flag = 1;
         }
@@ -76,22 +74,19 @@ int knights_tour_util(int x, int y, int move)
     return flag;
 }
 
-void init_board()
-{
-    int i, j;
-    for (i = 0; i < DIM; i++)
-        for (j = 0; j < DIM; j++)
-            board[i][j] = 0;
-}
-
-void solve_knights_tour()
+int main(int argc, char *argv[])
 {
     int i, j;
     int success = 0;
 
+    if (argc > 1 && argv[1] == 'v')
+        visual = 1;
+
     path = (int *)malloc(DIM * DIM * sizeof(int));
 
-    init_board();
+    for (i = 0; i < DIM; i++)
+        for (j = 0; j < DIM; j++)
+            board[i][j] = 0;
 
     for (i = 0; i < DIM; i++)
         for (j = 0; j < DIM; j++)
@@ -104,10 +99,5 @@ void solve_knights_tour()
         printf("No tours found\n");
 
     free(path);
-}
-
-int main(int argc, char *argv[])
-{
-    solve_knights_tour();
     return 0;
 }
