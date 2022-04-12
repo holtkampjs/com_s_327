632e2fcf0b499b5216744d08577e8cea2c53dc1d
josephh1 <josephh1@pyrite-n4.cs.iastate.edu>
2022-04-12 00:37:01 -0500

    Added screen where player chooses their pokemon

--- pokemon_1_08/io.h
+++ pokemon_1_08/io.h
@@ -11,5 +11,6 @@ void io_handle_input(pair_t dest);
 void io_queue_message(const char *format, ...);
 void io_battle(Character *aggressor, Character *defender);
 void io_encounter_pokemon(void);
+void io_choose_starting_pokemon(void);
 
 #endif
