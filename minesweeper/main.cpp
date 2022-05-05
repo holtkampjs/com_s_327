b2532a45ab89d798c8301ea39389740024ee3907
holtkampjs <holtkampjs@gmail.com>
2022-05-05 11:25:52 -0500

    Getting started on last assignment

---
+++ minesweeper/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ncurses.h>
+
+using namespace std;
+
+class MineSweeper {
+private:
+public:
+  MineSweeper() {}
+  ~MineSweeper() {}
+};
+
+int main(int argc, char *argv[]) {
+  MineSweeper *m = new MineSweeper();
+  return 0;
+}
